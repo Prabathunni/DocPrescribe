ded7f26329317e611807cfc942912eac502b939d
Prabath P U <prabathunni826@gmail.com>
2025-07-18 12:51:12 +0530

    Project deploy.4

--- backend/index.cjs
+++ backend/index.cjs
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser');
 const path = require('path')
 
 const app = express();
-PORT=process.env.PORT || 3000;
+const PORT=process.env.PORT || 3000;
 
 // cors middleware
 app.use(cors({
